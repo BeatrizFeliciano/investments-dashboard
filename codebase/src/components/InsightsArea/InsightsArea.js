edcca37d87bd76c969bae09defdc13e12f0a1b25
Beatriz Feliciano <beatriz.feliciano@tecnico.ulisboa.pt>
2025-02-28 23:24:28 +0000

    added x and y axis of investment rounds graph

--- codebase/src/components/InsightsArea/InsightsArea.js
+++ codebase/src/components/InsightsArea/InsightsArea.js
@@ -1,10 +1,56 @@
 import DemographicInfo from "./components/DemographicInfo";
 import "./InsightsArea.css";
-import { getEntityNumInvestments, getEntityTotalInvested } from "../../utils/getters";
+import { getEntityInvestmentsList, getEntityNumInvestments, getEntityTotalInvested, getInvestmentAmount, getInvestmentRound } from "../../utils/getters";
 import InformationCard from "./components/InformationCard";
 import { numberFormatter } from "../../utils/formatters";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import { useMemo } from "react";
 
 function InsightsArea({ selectedEntity }) {
+    const investmentsList = useMemo(() => getEntityInvestmentsList(selectedEntity), [selectedEntity]);
+    const investmentRounds = {};
+
+    investmentsList.forEach((investment) => {
+        const currentInvestment = investmentRounds[getInvestmentRound(investment)];
+
+        if (currentInvestment) {
+            investmentRounds[getInvestmentRound(investment)] = {
+                totalAmount: currentInvestment.totalAmount + getInvestmentAmount(investment),
+                investments: [...currentInvestment.investments, investment],
+            };
+
+        }
+        else {
+            investmentRounds[getInvestmentRound(investment)] = {
+                totalAmount: getInvestmentAmount(investment),
+                investments: [investment],
+            };
+        }
+    });
+
+    const maxAmount = 
+        Math.max(...Object.keys(investmentRounds).map(
+            (investmentKey) => investmentRounds[investmentKey].totalAmount));
+
+    const xScale = useMemo(
+        () => 
+            scaleBand({
+                range: [0, 5000], // TODO: define graph dimensions
+                domain: Object.keys(investmentRounds),
+            }), [maxAmount]
+    );
+
+    const yScale = useMemo(
+        () => 
+            scaleLinear({
+                range: [5000, 0], // TODO: define graph dimensions
+                domain: [0, maxAmount],
+                nice: true,
+            })
+    );
+    
+
+
     return (
         <div className="insights-area-wrapper">
             <div className="overview-marketsegments">
