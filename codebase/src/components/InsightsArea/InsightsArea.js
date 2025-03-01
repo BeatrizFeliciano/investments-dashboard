8ad0d68e6e7499b77c0bcbce4b081db52303a07f
Beatriz Feliciano <beatriz.feliciano@tecnico.ulisboa.pt>
2025-03-01 19:40:36 +0000

    added market and solution segment tables

--- codebase/src/components/InsightsArea/InsightsArea.js
+++ codebase/src/components/InsightsArea/InsightsArea.js
@@ -1,12 +1,16 @@
 import DemographicInfo from "./components/DemographicInfo";
-import { getEntityNumInvestments, getEntityTotalInvested } from "../../utils/getters";
+import { getEntityMarketSegments, getEntityNumInvestments, getEntitySolutionSegments, getEntityTotalInvested, getMarketSegmentName, getSolutionSegmentName } from "../../utils/getters";
 import InformationCard from "./components/InformationCard";
 import { numberFormatter } from "../../utils/formatters";
 import "./InsightsArea.css";
 import InvestmentsGraphs from "./components/InvestmentsGraphs/InvestmentsGraphs";
+import { Box, Grid, Typography } from "@mui/material";
+import { blueColor, BOX_SHADOW } from "../../constants";
 
 
-function InsightsArea({ selectedEntity }) {         
+function InsightsArea({ selectedEntity }) {  
+    const marketSegments = getEntityMarketSegments(selectedEntity);   
+    const solutionSegments = getEntitySolutionSegments(selectedEntity);    
     
     return (
         <div className="insights-area-wrapper">
@@ -24,7 +28,46 @@ function InsightsArea({ selectedEntity }) {
                         />
                     </div>
                 </div>
-                <div style={{ width: "100%" }}>Tables will be here</div>
+                <div className="investment-areas-wrapper">
+                    {marketSegments && <Box 
+                        className="investment-areas"
+                        component={Grid}
+                        direction="column"
+                        container
+                        boxShadow={BOX_SHADOW}
+                    >
+                        <Typography 
+                            className="investment-areas-title" 
+                            color={blueColor} 
+                            fontWeight="bold"
+                            align="left"
+                        >
+                            Market Segments
+                        </Typography>
+                        <Typography 
+                            className="investment-areas-list"
+                            align="left"
+                        >
+                            {getEntityMarketSegments(selectedEntity).map((marketSegment) => (
+                                <div className="area-investment">{getMarketSegmentName(marketSegment)}</div>
+                            ))}
+                        </Typography>
+                    </Box>}
+                    {solutionSegments && <Box 
+                        className="investment-areas"
+                        component={Grid}
+                        direction="column"
+                        container
+                        boxShadow={BOX_SHADOW}
+                    >
+                        <div className="investment-areas-title">Solution Segments</div>
+                        <div className="investment-areas-list">
+                            {solutionSegments.map((solutionSegment) => (
+                                <div className="area-investment">{getSolutionSegmentName(solutionSegment)}</div>
+                            ))}
+                        </div>
+                    </Box>}
+                </div>
             </div>
             <InvestmentsGraphs selectedEntity={selectedEntity}/>
         </div>
