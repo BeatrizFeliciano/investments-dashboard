b7faaa66a2af41e55bf5351f26838622cd5230bd
Beatriz Feliciano <beatriz.feliciano@tecnico.ulisboa.pt>
2025-03-02 00:06:31 +0000

    add entity demographic icons

--- codebase/src/components/InsightsArea/components/DemographicInfo.js
+++ codebase/src/components/InsightsArea/components/DemographicInfo.js
@@ -1,14 +1,27 @@
-import { Box, Grid, Typography } from "@mui/material";
-import "../InsightsArea.css";
+import { Typography } from "@mui/material";
+import { IoPersonOutline } from "react-icons/io5";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { FaRegBuilding } from "react-icons/fa";
 import { getEntityHqCountry, getEntityName, isEntity } from "../../../utils/getters";
+import "../InsightsArea.css";
 
 function DemographicInfo({ selectedEntity }) {
     return (
         <div className="demographic-info">
-            <Typography>
-                {getEntityName(selectedEntity)}
-                {isEntity(selectedEntity) ? `, ${getEntityHqCountry(selectedEntity)}` : ""}
-            </Typography>
+            <div className="demographic-info-element">
+                {isEntity(selectedEntity) ? <FaRegBuilding size={25}/> : <IoPersonOutline size={25}/>}
+                <Typography variant="h6" fontWeight="bold">
+                    {getEntityName(selectedEntity)}
+                </Typography>
+            </div>
+            {isEntity(selectedEntity) && 
+                <div className="demographic-info-element">
+                    <MdOutlineLocationOn size={25}/>
+                    <Typography variant="h6">
+                        {getEntityHqCountry(selectedEntity)}
+                    </Typography>
+                </div>
+            }
         </div>
     );
 }
